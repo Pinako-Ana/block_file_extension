0f93e96ce449336937a86d4171b678d63765e954
Pinako_Ana <fufru@naver.com>
2023-02-25 14:48:34 +0900

    add .gitignore

---
+++ src/main/java/com/flow/extension_block/ExtensionBlockApplication.java
@@ -0,0 +1,13 @@
+package com.flow.extension_block;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtensionBlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtensionBlockApplication.class, args);
+	}
+
+}
