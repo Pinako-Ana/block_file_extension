ace1f3f97855535f1e5752ee4d19589717df0029
Pinako_Ana <fufru@naver.com>
2023-02-26 15:24:09 +0900

    [feat] 커스텀 확장자 조회 레포지토리 메소드 생성

--- src/main/java/com/flow/extension_block/domain/extension/repository/CustomExtensionRepository.java
+++ src/main/java/com/flow/extension_block/domain/extension/repository/CustomExtensionRepository.java
@@ -5,9 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface CustomExtensionRepository extends JpaRepository<CustomExtension, Integer> {
 
     @Query("SELECT COUNT(c) > 0 FROM CustomExtension c WHERE c.extensionName = :extensionName AND c.isDeleted = FALSE")
     boolean existsByExtensionName(@Param("extensionName") String extensionName);
+
+    @Query("SELECT c FROM CustomExtension c WHERE c.isDeleted = FALSE")
+    List<CustomExtension> findCustomExtensions();
 
 }
